472a6506460aa25a3511f1f8bc3aa3ec487f5e11
Adam Thomas <thomasadam83@hotmail.com>
2022-10-04 09:36:00 -0400

    Deacticated Twitter embed

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -1,18 +1,19 @@
-import React,{Component} from "react";
+import React, { Component } from 'react';
 
-import TweetEmbed from 'react-tweet-embed'
+// import TweetEmbed from 'react-tweet-embed'
 
 class Twitter extends Component {
   render() {
     return (
-      <div className={"Twitter w3-padding-32 w3-" + (this.props.colour ? this.props.colour : "white")}>
-        
+      <div
+        className={
+          'Twitter w3-padding-32 w3-' +
+          (this.props.colour ? this.props.colour : 'white')
+        }
+      >
         <div className="ca-container-550 w3-center">
-
-          <TweetEmbed id={this.props.id}></TweetEmbed>
-
+          {/*<TweetEmbed id={this.props.id}></TweetEmbed>*/}
         </div>
-
       </div>
     );
   }
